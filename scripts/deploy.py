bcbc94467f2e4bef32cf81a077990eb0442628c4
techwithnaga <techwithnaga@gmail.com>
2021-12-28 19:04:59 -0600

    adding default account

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,11 +3,11 @@ from brownie import accounts
 # def deployDefaultAccount():
 #     print(accounts[0])
 
+
 def deployRinkebeTestAccount():
     account = accounts.load("rinkeby_test_account")
     print(account)
 
 
 def main():
-    deployDefaultAccount()
-
+    deployRinkebeTestAccount()
